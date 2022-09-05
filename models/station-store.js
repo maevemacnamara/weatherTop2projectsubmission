5b2113f83fff40b229641145d846f57caa935553
Glitch (toothsome-languid-seismosaurus) <none>
2022-09-05 20:38:32 +0000

    🍰👸 Checkpoint ./views/dashboard.hbs:48299489/22 ./models/station-store.json:48299489/902 ./models/station-store.js:48299489/768

--- models/station-store.js
+++ models/station-store.js
@@ -1,47 +1,5 @@
 'use strict'
 
-const logger = require('../utils/logger');
-
-const lucan = {
-  title: 'Lucan',
-  readings: [
-    {
-      code: '100',
-      temp: '25',
-      windSpeed:'11',
-      windDirection:'NNE',
-      pressure:'1000',
-    },
-    {
-      code: '200',
-      temp: '14',
-      windSpeed:'22',
-      windDirection:'SWS',
-      pressure:'800',
-    },
-  ],
-};
-
-const leixlip = {
-  title: 'Leixlip',
-  readings: [
-    {
-      code: '200',
-      temp: '10',
-      windSpeed:'28',
-      windDirection:'WSW',
-      pressure:'900',
-    },
-    {
-      code: '300',
-      temp: '18',
-      windSpeed:'05',
-      windDirection:'ENE',
-      pressure:'600',
-    },
-  ],
-};
-
-const stationCollection = [lucan, leixlip];
+const stationCollection = require('./station-store.json').stationCollection;
 
 module.exports = stationCollection;
