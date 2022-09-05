984ca8fd992b2330691bf6fa47922d0dc104c230
Glitch (toothsome-languid-seismosaurus) <none>
2022-09-05 21:26:29 +0000

    🐥👱 Checkpoint ./controllers/station.js:48299489/390 ./views/station.hbs:48299489/132 ./routes.js:48299489/167 ./models/station-store.js:48299489/371

--- controllers/station.js
+++ controllers/station.js
@@ -1,4 +1,18 @@
 'use strict';
 
-c
+const logger = require('../utils/logger');
+const stationCollection = require('../models/station-store.js');
+
+const station = {
+  index(request, response) {
+    const stationId = request.params.id;
+    logger.info('Station id = ' + stationId);
+    const viewData = {
+      title: 'Station',
+    };
+    response.render('station', viewData);
+  },
+};
+
+module.exports = station;
 
--- models/station-store.js
+++ models/station-store.js
@@ -1,5 +1,22 @@
 'use strict'
 
-const stationCollection = require('./station-store.json').stationCollection;
+const stationStore = {
+
+    stationCollection: require('./station-store.json').stationCollection,
+      
+    getAllStations(){
+      return this.stationCollection;
+    },
+
+    getStation(id) {
+      let foundStation = null;
+      for (let station of this.stationCollection){
+        if (id == station.id) {
+          foundStation = station;
+        }
+      }
+      return foundStation;
+    },
+};
 
 module.exports = stationCollection;
