67d0251fc41f44b928c73e610370c5308dcf7e3b
Glitch (toothsome-languid-seismosaurus) <none>
2022-09-09 13:54:14 +0000

    👵🌆 Checkpoint ./routes.js:48299489/563 ./views/partials/menu.hbs:48299489/243 ./controllers/dashboard.js:48299489/309

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -3,13 +3,15 @@
 const logger = require("../utils/logger");
 const stationStore = require("../models/station-store");
 const uuid = require('uuid');
+const accounts = require ('./accounts.js');
 
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
+    const loggedInUser = accounts.getCurrentUser(request);
     const viewData = {
       title: "Station Dashboard",
-      station: stationStore.getAllStations(),
+      station: stationStore.getUserStations(loggedInUser.id),
     };
     logger.info('about to render', stationStore.getAllStations());
     response.render("dashboard", viewData);
@@ -23,11 +25,14 @@ const dashboard = {
   },
   
   addStation(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
     const newStation = {
       id: uuid.v1(),
+      userid: loggedInUser.id,
       title: request.body.title,
       readings: [],
     };
+    logger.debug('Creating a new Station', newStation);
     stationStore.addStation(newStation);
     response.redirect('/dashboard');
   }
