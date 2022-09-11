4318f43b58e411460fdc729b15fae73285efeb11
Glitch (toothsome-languid-seismosaurus) <none>
2022-09-11 08:24:49 +0000

    👩‍❤️‍💋‍👩🌪 Checkpoint ./utils/reading-conversions.js:48299489/455

--- utils/reading-conversions.js
+++ utils/reading-conversions.js
@@ -5,9 +5,19 @@ const readingConversions = {
   },
   
   getWeatherCode(code) {
-    const weatherCodes = new Map();
-    weatherc
-  }
+    const weatherCode = new Map();
+    weatherCode.set('100', 'Clear');
+    weatherCode.set('200', 'Partial Clouds');
+    weatherCode.set('300', 'Cloudy');
+    weatherCode.set('400', 'Light Showers');
+    weatherCode.set('500', 'Heavy Showers');
+    weatherCode.set('600', 'Rain');
+    weatherCode.set('700', 'Snow');
+    weatherCode.set('800', 'T-storm');
+    return weatherCode.get(code);
+  },
+  
+  
 };
 
 
